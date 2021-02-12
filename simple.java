73e744646e3196e92c0d65d0627cf6e2a1d75cdc
big <kilonzoally@gmail.com>
2021-02-12 14:17:34 +0300

    initial data

---
+++ simple.java
@@ -0,0 +1,27 @@
+
+class hello{
+
+	//int a =8;
+
+     public static void main(String[] args) {
+     	// hello b = new hello();
+     	// int c = b.a;
+
+     	//Widening
+     	int a = 20;
+     	float f = a;
+         
+         System.out.println(f);
+     	//System.out.println("HelloWorld" +c);
+
+         //typecasting
+           float x = 10.7f;
+
+           int c = (int)x;
+           System.out.println(c);
+
+           
+}
+     	
+        
+    }
